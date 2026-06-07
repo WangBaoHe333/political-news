0abac6bddc3a9cbfec66b86a7964f2b6837e3467
WangBaoHe333 <2280200176@qq.com>
2026-06-07 08:20:18 +0800

    Fix: Add return type annotation to init_db function in database.py

--- app/database.py
+++ app/database.py
@@ -14,5 +14,5 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def init_db():
+def init_db() -> None:
     Base.metadata.create_all(bind=engine)
